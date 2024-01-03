aeb9ca498344d995da8e74fc82dcf0cdedeb750f
Emma <65368545+electricblues@users.noreply.github.com>
2024-01-03 22:19:58 +0000

    added colour inputs to change individual colours

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ function generatePalette() {
 
   for (let i = 0; i < 5; i++) {
     const colour = generateRandomColor();
+
+    // create colorbox
     const colorBox = document.createElement("div");
     colorBox.classList.add("colour");
     colorBox.style.backgroundColor = colour;
+
+    // create colour text
+    const colourText = document.createElement("div");
+    colourText.innerHTML = colour;
+
+    // Create a color input element
+    const colorInput = document.createElement("input");
+    colorInput.classList.add("colour-input");
+    colorInput.type = "color";
+    colorInput.value = colour;
+
+    colorInput.addEventListener("input", function () {
+      colorBox.style.backgroundColor = this.value;
+      colourText.innerHTML = this.value;
+    });
+
+    // Append the color input to the color box
+    colorBox.appendChild(colorInput);
+    colorBox.appendChild(colourText);
+
     palette.appendChild(colorBox);
   }
 }
